e18ae78c3245dc3190073a10080b8106f0c800ca
Kuda11 <kudathomad17@yahoo.com>
2020-10-14 16:25:47 +0100

    Sign-up file framwork complete

---
+++ src/components/sign-up/register/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./Register.module.scss"
+
+export const Register = () => {
+  return (
+    <>
+      <h1>Dada</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt id ex nobis dicta porro odit numquam maxime, quisquam qui modi quibusdam, nulla quaerat rerum delectus ipsum repudiandae quae dignissimos! Dolorem.</p>
+    </>
+  )
+}
+
+
+export default Register;
