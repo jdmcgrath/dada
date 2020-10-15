6d2ea83ff9a879219cb654d257a8b24ad39478dc
Joseph McGrath <joseph.mcgrath@engineer.com>
2020-10-15 15:43:20 +0100

    Fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
-
+import Categories from "./components/Categories"
 export const App = () => {
   return (
     <>
-    <nav>
-      <h3>first section</h3>
-      <h3>second section</h3>
-      <h3>third section</h3>
-
-    </nav>
-      <h1>Add Chick</h1>
-      <img className="roundedImg" src="https://static.scientificamerican.com/sciam/cache/file/92E141F8-36E4-4331-BB2EE42AC8674DD3_source.jpg" alt=""/>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt id ex nobis dicta porro odit numquam maxime, quisquam qui modi quibusdam, nulla quaerat rerum delectus ipsum repudiandae quae dignissimos! Dolorem.</p>
-      <form action="">Hello my name is joe
-      <input type="text"/></form>
-      <button> Submit</button>
+      <h1>Dada</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Incidunt id ex nobis dicta porro odit numquam maxime, 
+        quisquam qui modi quibusdam, nulla quaerat rerum delectus 
+        ipsum repudiandae quae dignissimos! Dolorem.</p>
+        <Categories />
     </>
   )
-};
-
-
+}
 export default App;
