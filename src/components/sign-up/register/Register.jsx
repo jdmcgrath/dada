f840194e622718175d3444dedbe35e4f8755e8e1
Kuda11 <kudathomad17@yahoo.com>
2020-10-15 15:09:01 +0100

    Html form layout

--- src/components/sign-up/register/Register.jsx
+++ src/components/sign-up/register/Register.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import styles from "./Register.module.scss"
 
+
+
+
 export const Register = () => {
   return (
-    <>
-      <h1>Dada</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt id ex nobis dicta porro odit numquam maxime, quisquam qui modi quibusdam, nulla quaerat rerum delectus ipsum repudiandae quae dignissimos! Dolorem.</p>
-    </>
+    <div className="base-container">
+      <div className="header">Sign Up</div>
+      <div className="content">
+        <div className="form">
+          <div className="form-group">
+            <input type="text" placeholder="Name"/>
+            </div>
+          <div className="form-group">
+            <input type="email" placeholder="Email"/>
+            </div>
+          <div className="form-group">
+            <input type="password" placeholder="password"/>
+            </div>
+          <p>I read and agree to <strong>Terms and Condition</strong></p>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Sign Up
+          </button>
+        </div>
+        <p>Already have an account? <a>Login</a></p>
+        <p>or sign in with</p>
+      </div>
+    </div>
   )
 }
 
