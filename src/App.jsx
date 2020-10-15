8557a254d99334b85ca361f3fe1a9ac518912a3d
Frank McNulty <frank.mcnulty@nology.io>
2020-10-15 15:45:13 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import AddChick from "./addChick/AddChick"
 export const App = () => {
   return (
     <>
-      <h1>Dada</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt id ex nobis dicta porro odit numquam maxime, quisquam qui modi quibusdam, nulla quaerat rerum delectus ipsum repudiandae quae dignissimos! Dolorem.</p>
+      <AddChick />
     </>
-  )
+  ) 
 }
 
 
