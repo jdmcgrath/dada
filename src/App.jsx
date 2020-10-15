11b3cd8cc35c11d3e1d04ac295f43d504ea0d357
Kuda11 <kudathomad17@yahoo.com>
2020-10-15 15:21:19 +0100

    Live sever bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Register from "./components/sign-up/Register"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const App = () => {
   return (
     <>
