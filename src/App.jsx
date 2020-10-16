51af1a47badfff1515445bfa59608178d504d250
Mark Birchall <mark.birchall@gmail.com>
2020-10-16 16:15:01 +0100

    beginning navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import AddChick from "./addChick/AddChick"
 import Categories from "./components/Categories/Categories"
+import NavBar from "./components/NavBar/NavBar"
+
+
+
 export const App = () => {
   return (
     <>
-        <Categories />
+        <NavBar />
     </>
   )
 }
---
+++ src/components/data/fa-library.js
@@ -0,0 +1,9 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
+
+
+
+export default library.add( 
+    faEllipsis
+);
