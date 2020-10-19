bd8d79024c9b73d5f32d410585157d7714113a65
Mark Birchall <mark.birchall@gmail.com>
2020-10-19 16:26:17 +0100

    feature 7to1 3rd attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import AddChick from "./addChick/AddChick";
-import Categories from "./components/Categories/Categories";
-import NavBar from "./components/NavBar/NavBar";
-
-
-
-=======
 import Categories from "./components/Categories/Categories"
->>>>>>> cad8c22a2ddad4c798f2a4212b448d22040b8df1
 export const App = () => {
   return (
     <>
