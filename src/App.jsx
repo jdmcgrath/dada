6b170f96a320381e945de2ac7ed80dc93ddc9f06
Steve Maguire <dannyblack42@protonmail.com>
2020-10-19 16:19:12 +0100

    feat: create folders & files for activity-ideas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import AddChick from "./components/addChick/AddChick"
 import Categories from "./components/Categories/Categories"
+import Register from './components/sign-up/register';
+
 export const App = () => {
   return (
     <>
         <Categories />
+        {/* <h1>Dada</h1> */}
+        {/* <Categories /> */}
+        <AddChick />
     </>
   )
 }
