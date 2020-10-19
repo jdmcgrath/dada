5a10ec05c95149c483f1f5a167039784d838fe98
daniel evans <daniel.jj.evans1@gmail.com>
2020-10-19 15:23:49 +0100

    Added a containers/Routing folder and added files

---
+++ src/containers/Routing/Routing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Router } from "@reach/router";
+
+const Routing = () => {
+    return(
+        <Router>
+            
+        </Router>
+    )
+}
+
+export default Routing;
