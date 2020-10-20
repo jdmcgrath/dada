daf2e66e0cf8798edac1bdb1e9fefac41b598cf1
Kuda11 <kudathomad17@yahoo.com>
2020-10-20 16:32:48 +0100

    Sign up page complete

--- src/components/sign-up/register/Register.jsx
+++ src/components/sign-up/register/Register.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from "./Register.module.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SocialFollow from './SocialFollow';
 
 //Font Awesome:
 
@@ -10,11 +11,7 @@ export const Register = () => {
 
 
   return (
-<<<<<<< HEAD
     <article className={styles.baseContainer}>
-=======
-    <article className={styles.pageContainer}>
->>>>>>> c308c185664b376dc3d8745bbdd34fb3d67d05a2
       <div className={styles.header}>Sign Up</div>
       <div className={styles.content}>
         <div className={styles.form}>
@@ -25,11 +22,7 @@ export const Register = () => {
             <input type="email" placeholder="Email"/>
             </div>
           <div className={styles.formGroup}>
-<<<<<<< HEAD
             <input type="password" placeholder="Password"/>
-=======
-            <input type="password" placeholder="password"/>
->>>>>>> c308c185664b376dc3d8745bbdd34fb3d67d05a2
             </div>
           <p className={styles.terms}>I read and agree to <strong>Terms and Condition</strong></p>
         </div>
@@ -40,7 +33,7 @@ export const Register = () => {
         </div>
         <p className={styles.terms}>Already have an account? <strong><a>Login</a></strong></p>
         <p className={styles.orSignIn}>or sign in with</p>
-       <i className="fab "></i><i className="fab fa-twitter"></i><i className="fab fa-google"></i>
+        < SocialFollow />
       </div>
     </article>
   )
