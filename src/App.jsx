a2a1d16dab24e8610388cb9adf77fa32e91734e0
Frank McNulty <frank.mcnulty@nology.io>
2020-10-20 14:18:42 +0100

    fix conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
 import AddChick from "./components/addChick/AddChick"
 import Categories from "./components/Categories/Categories"
-<<<<<<< HEAD
 import InputData from "./components/data/Input.js";
 // import Input from "./components/Input";
 // import library from "./components/data/fa-library";
 // import FloatPlaceHolder from "./components/FloatPlaceHolder.module.scss";
 
 
-=======
 import Register from './components/sign-up/register';
->>>>>>> c308c185664b376dc3d8745bbdd34fb3d67d05a2
 
 export const App = () => {
   return (
     <>
-<<<<<<< HEAD
         <AddChick InputData={InputData} />
         {/* <Input InputData={InputData} /> */}
-=======
 
         <Categories />
         {/* <h1>Dada</h1> */}
         {/* <Categories /> */}
         <AddChick />
->>>>>>> c308c185664b376dc3d8745bbdd34fb3d67d05a2
     </>
   )
 }
--- src/components/data/fa-library.js
+++ src/components/data/fa-library.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import { library } from "@fortawesome/fontawesome-svg-core";
 
 // import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
@@ -6,7 +5,6 @@
 // export default library.add(
 //     faPlusCircle
 // )
-=======
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
@@ -16,4 +14,3 @@ import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 export default library.add( 
     faEllipsis
 );
->>>>>>> c308c185664b376dc3d8745bbdd34fb3d67d05a2
