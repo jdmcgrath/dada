9234dc802f34eeb8cbadb07e2fbdee9af04ffa21
daniel evans <daniel.jj.evans1@gmail.com>
2020-10-21 16:57:56 +0100

    fixed route paths and imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import AddChick from "./components/addChick/AddChick"
 import Categories from "./components/Categories/Categories"
 import InputData from "./components/data/Input.js";
 import BookSmarts from "./components/bookSmarts/BookSmarts";
-import Routes from "./containers/Routes/Routing";
+import Routes from "./containers/Routes/Routes";
+import Register from "./components/sign-up/register"
+import { Router, Link } from "@reach/router"
+
+
+
 // import Input from "./components/Input";
 // import library from "./components/data/fa-library";
 // import FloatPlaceHolder from "./components/FloatPlaceHolder.module.scss";
@@ -14,23 +19,18 @@ import Routes from "./containers/Routes/Routing";
 export const App = () => {
   return (
     <>
+        <Routes />
 
-<div className='App'>
-        <Route path="/addchick" component={AddChick} />
-        <Route path="/categories" component={Categories} />
-        <Route path="/register" component={Register} />
-
-      </div>
     
 
         {/* { <AddChick InputData={InputData} /> } */}
         {/* {/<Input InputData={InputData} /> } */}
-        <BookSmarts />
+        {/* <BookSmarts /> */}
         {/* {<Categories /> } */}
         {/* { <h1>Dada</h1> } */}
         {/* {<Categories /> } */}
         {/* {<AddChick /> } */}
-        <Routes />
+        {/* <Routes /> */}
     </>
   )
 }
