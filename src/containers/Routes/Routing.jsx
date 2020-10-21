0be4f81aead806b6eb83f9be12f8ddab9d52cfc1
daniel evans <daniel.jj.evans1@gmail.com>
2020-10-21 14:28:51 +0100

    commiting

--- src/containers/Routes/Routing.jsx
+++ src/containers/Routes/Routing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Router } from "@reach/router";
+
 import Categories from "../../components/Categories";
 import SignUp from "../../components/sign-up";
 import AddChick from "../../addChick";
@@ -8,7 +9,7 @@ import VideoList from "../../components/Categories/VideoList";
 import ArticleList from "../../components/Categories/ArticleList";
 
 
-const Routing = () => {
+const Routes = () => {
     return(
         <Router>
             <Categories path="categories" />
