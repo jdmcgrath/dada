ada87ff96164e553b00ec1303410b187058ba3af
James <james.loizou@nology.io>
2020-10-22 16:03:44 +0100

    needs positioning

--- src/components/SplashScreen/SplashScreen.jsx
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -5,12 +5,12 @@ import splash from "../../assets/images/dada-splash-large.png"
 
 const SplashScreen = () => {
     return (
-        <div className={styles.pageContainer}>
+        <div className={styles.pageContainer} className={styles.splashPage}>
         <div className={styles.splash} >
             <img src={splash} alt="splash"/>
             <p>Parenting Coach in Your Pocket</p>
         </div>
-        <div>
+        <div className={styles.pageBottom}>
             <button>Get Dadvice</button>
             <p>Already have an account? <span>Log in</span> or <span>Sign up</span></p>
         </div>
