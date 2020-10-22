b3753135a6270b3e930658b4b28fdc75cc97bc7b
Mark Birchall <mark.birchall@gmail.com>
2020-10-22 14:31:51 +0100

    sos-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import SOS from "./components/Categories/SOS/SOS";
 import AddChick from "./components/addChick/AddChick"
 import Categories from "./components/Categories/Categories"
-import Register from './components/sign-up/register'
-import NavBar from './components/NavBar'
+import InputData from "./components/data/Input.js";
+import BookSmarts from "./components/bookSmarts/BookSmarts";
+import Routes from "./containers/Routes/Routes";
+import Register from "./components/sign-up/register"
+import { Router, Link } from "@reach/router"
+
+
+
+// import Input from "./components/Input";
+// import library from "./components/data/fa-library";
+// import FloatPlaceHolder from "./components/FloatPlaceHolder.module.scss";
+
+
+// import Register from './components/sign-up/register';
 
 export const App = () => {
   return (
     <>
-    <NavBar />
-        <SOS />
-        {/* <Categories /> */}
-        {/*<AddChick />*/}
+        <Routes />
+
+    
+
+        {/* { <AddChick InputData={InputData} /> } */}
+        {/* {/<Input InputData={InputData} /> } */}
+        {/* <BookSmarts /> */}
+        {/* {<Categories /> } */}
+        {/* { <h1>Dada</h1> } */}
+        {/* {<Categories /> } */}
+        {/* {<AddChick /> } */}
+        {/* <Routes /> */}
     </>
   )
 }
