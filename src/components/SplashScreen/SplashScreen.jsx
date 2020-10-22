e0a5c4adc7cd8014ba311c9baf76a2125cc5a980
James <james.loizou@nology.io>
2020-10-22 15:08:37 +0100

    first commit

---
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./SplashScreen.module.scss"
+
+const SplashScreen = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SplashScreen
