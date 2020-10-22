99285bde667f2dac20285aa8d54faadccf9fc9a1
Mark Birchall <mark.birchall@gmail.com>
2020-10-22 15:40:02 +0100

    adding image

--- src/components/SplashScreen/SplashScreen.jsx
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -5,7 +5,7 @@ import splash from "../../assets/images/dada-splash-large.png"
 
 const SplashScreen = () => {
     return (
-        <>
+        <div className={styles.pageContainer}>
         <div className={styles.splash} >
             <img src={splash} alt="splash"/>
             <p>Parenting Coach in Your Pocket</p>
@@ -14,7 +14,7 @@ const SplashScreen = () => {
             <button>Get Dadvice</button>
             <p>Already have an account? <span>Log in</span> or <span>Sign up</span></p>
         </div>
-        </>
+        </div>
     )
 }
 
