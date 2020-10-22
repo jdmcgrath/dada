fd54dcd15ba2ac22dca8253d54d5e211f60156f1
Mark Birchall <mark.birchall@gmail.com>
2020-10-22 14:28:46 +0100

    sos-again

--- src/components/Categories/CategoryCards/CategoryCards.jsx
+++ src/components/Categories/CategoryCards/CategoryCards.jsx
@@ -1,10 +1,7 @@
-// category cards folder can be removed
+import React from "react";
+import styles from "./CategoryCards.module.scss";
 
-
-// import React from "react";
-// import styles from "./CategoryCards.module.scss";
-
-// export default CategoryCards;
+export default CategoryCards;
 
 
 
