033d5b3a3aca31644082fdef2da8eb4ed8534592
Mark Birchall <mark.birchall@gmail.com>
2020-10-22 15:38:06 +0100

    adding image

--- src/components/SplashScreen/SplashScreen.jsx
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from "./SplashScreen.module.scss"
+import splash from "../../assets/images/dada-splash-large.png"
+
 
 const SplashScreen = () => {
     return (
         <>
-        <div>
-            <img src="../../assets/images/dada-splash-large.png" alt=""/>
+        <div className={styles.splash} >
+            <img src={splash} alt="splash"/>
             <p>Parenting Coach in Your Pocket</p>
         </div>
         <div>
