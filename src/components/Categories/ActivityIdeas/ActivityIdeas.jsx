ba77d7c7b3b42d8c7a1a3ddc624629ef9ac05812
Steve Maguire <dannyblack42@protonmail.com>
2020-10-23 15:18:35 +0100

    feat: add toggle between vid list & act list

--- src/components/Categories/ActivityIdeas/ActivityIdeas.jsx
+++ src/components/Categories/ActivityIdeas/ActivityIdeas.jsx
@@ -11,26 +11,24 @@ import videos from '../../data/videos';
 const ActivityIdeas = () => {  
 
 
-  const [toggleList, setToggleList] = useState(false);
+  const [toggleList, setToggleList] = useState(true);
 
-  const setList = () => { 
-    setToggleList(!toggleList);
-  }
+  const showSomething = toggleList ? <VideoList videos={videos} id="videoList" className={styles.videoList} /> : <ArticleList articles={articles} id="articleList" className={styles.articleList} />
 
-import {Link} from '@reach/router';
+
 
   return (
     //   <nav>Top Navbar</nav>
     <div className={styles.activityIdeas}>
-        {/* Buttons to select whether you wish to view a video or article */}
-      <Link to = "video-list" ><button>Videos</button></Link>
-      <Link to = "article-list"><button>Articles</button></Link>
+
+    
+        <button onClick={() => setToggleList(true)}>Videos</button>
+      <button onClick={() => setToggleList(false)}>Articles</button>
 
     <div className="filter">
       {/* Tabbed buttons to click for filtered lists based on keywords */}
     </div>
-      <VideoList  videos={videos} id="videoList" className={styles.videoList} toggleList={toggleList}/>
-      <ArticleList  articles={articles} id="articleList" className={styles.articleList} toggleList={toggleList}/>
+      {showSomething}
     </div>
   )
 }
