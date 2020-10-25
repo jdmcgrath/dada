a264fadbf05a996a708c4ed106f8335bbc7fd62d
Mark Birchall <mark.birchall@gmail.com>
2020-10-25 13:25:36 +0000

    restructure 7

--- src/components/bookSmarts/BookSmarts.jsx
+++ src/components/bookSmarts/BookSmarts.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./BookSmarts.module.scss";
 import BookSmartCard from "./BookSmartCard";
-import cardData from "./data"
+import cardData from "../../data/bookSmartData";
 
 const BookSmarts = () => {
 
