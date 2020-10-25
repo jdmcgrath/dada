7917639614214e66af73230cb9a7c00bceef6501
Mark Birchall <mark.birchall@gmail.com>
2020-10-25 13:31:10 +0000

    restructure 9

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -7,7 +7,7 @@ import ActivityIdeas from "../../components/ActivityIdeas";
 import VideoList from "../../components/VideoList";
 import ArticleList from "../../components/ArticleList";
 // import BookSmarts from "../../components/BookSmarts";
-import AddChick from "../../components/AddChick";
+// import AddChick from "../../components/AddChick";
 import SOS from "../../components/SOS";
 
 const Routes = () => {
@@ -15,7 +15,7 @@ const Routes = () => {
         <Router>
             <Categories path="categories" />
             <SignUp path="sign-up" />
-            <AddChick path="add-chick" />
+            {/* <AddChick path="add-chick" /> */}
             <ActivityIdeas path="categories/activity-ideas" />
             <VideoList path="categories/activity-ideas/video-list" />
             <ArticleList path="categories/activity-ideas/article-list" />
