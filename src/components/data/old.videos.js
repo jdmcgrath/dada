a60731889b21fceba343455b4af666a29b474f7b
Steve Maguire <dannyblack42@protonmail.com>
2020-10-26 15:05:20 +0000

    folder names

---
+++ src/components/data/old.videos.js
@@ -0,0 +1,55 @@
+const videos = [ 
+  
+  {
+  
+    "vidID": 1, 
+    "source": "YouTube",
+    "url": 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
+    "keyword": 'chilled'
+  
+  },
+  
+  {
+      
+    "vidID": 2, 
+    "source": "YouTube",
+    "url": 'https://www.youtube.com/watch?v=lTRiuFIWV54',
+    "keyword": 'study'
+    
+  },
+  
+  {
+      
+    "vidID": 3, 
+    "source": "YouTube",
+    "url": 'https://www.youtube.com/watch?v=sjkrrmBnpGE',
+    "keyword": 'focus'
+      
+  },
+  
+  {
+      
+    "vidID": 4, 
+    "source": "YouTube",
+    "url": 'https://www.youtube.com/watch?v=oaMfFb-5Qh4',
+    "keyword": 'relax'
+        
+  },
+  
+  {
+    "vidID": 5, 
+    "source": "YouTube",
+    "url": 'https://www.youtube.com/watch?v=1ZYbU82GVz4',
+    "keyword": 'sleep'
+  },
+
+  {
+    "vidID": 6, 
+    "source": "Vimeo",
+    "url": 'https://vimeo.com/95710885',
+    "keyword": 'fatherhood'
+  }
+  
+  ]
+  
+  export default videos
