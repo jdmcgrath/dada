40cd55db5be1158f6d40b0d15826d88362f8027b
daniel evans <daniel.jj.evans1@gmail.com>
2020-10-27 14:20:25 +0000

    fixed up your game routing

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -7,23 +7,8 @@ import ActivityIdeas from "../../components/ActivityIdeas";
 import BookSmarts from "../../components/BookSmarts";
 import AddChick from "../../components/AddChick";
 import SOS from "../../components/SOS";
-<<<<<<< HEAD
-import UpYourGame from "../../components/UpYourGame";
-
-const Routes = () => {
-    return(
-        <Router>
-            <Categories path="categories" />
-            <SignUp path="sign-up" />
-            <AddChick path="add-chick" />
-            <ActivityIdeas path="categories/activity-ideas" />
-            <BookSmarts path="book-smarts" />
-            <SOS path="sos"/>
-            <UpYourGame path ="up-your-game" />
-        </Router>
-    )
-=======
 import SplashScreen from "../../components/SplashScreen";
+import UpYourGame from "../../components/UpYourGame";
 
 const Routes = () => {
     return (
@@ -37,9 +22,9 @@ const Routes = () => {
         <BookSmarts path="book-smarts" />
         <SOS path="sos" />
         <SplashScreen path="splash" />
+        <UpYourGame path ="up-your-game" />
       </Router>
     );
->>>>>>> 3183b1c15f3c943c03a76f4d020023501300944c
 }
 
 export default Routes;
