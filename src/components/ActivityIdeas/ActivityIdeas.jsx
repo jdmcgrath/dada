a6a2e4306504a69375c403ec4bee9cc55ad28efc
Steve Maguire <dannyblack42@protonmail.com>
2020-10-28 17:03:55 +0000

    feat: add filter tag list

--- src/components/ActivityIdeas/ActivityIdeas.jsx
+++ src/components/ActivityIdeas/ActivityIdeas.jsx
@@ -7,55 +7,49 @@ import videos from "../../data/videos";
 import { Link } from '@reach/router';
 
 const ActivityIdeas = () => {
+
   const [toggleList, setToggleList] = useState(true);
 
-    const showClickedList = toggleList ? <VideoList videos={videos} /> : <ArticleList articles={articles} />
-
-    // const buttonStyle = toggleList ? styles.activeButton : styles.inactiveButton;
-    
-    // const showButtons = () => {
-
-    //   if (toggleList) {
-    //     return (
-    //       <>
-    //         <button className={styles.activeButton} onClick={() => setToggleList(true)}>Videos</button>
-    //         <button className={styles.inactiveButton} onClick={() => setToggleList(false)}>Articles</button>
-    //       </>
-    //     )
-    //   } else {
-    //     return (
-    //       <>
-    //         <button className={styles.inactiveButton} onClick={() => setToggleList(true)}>Videos</button>
-    //         <button className={styles.activeButton} onClick={() => setToggleList(false)}>Articles</button>
-    //       </>
-    //     )
-    //   }
-  
-    // }
+    const showClickedList = toggleList ? 
+    <VideoList videos={videos} /> : 
+    <ArticleList articles={articles} />
 
     const showButtons = toggleList ? 
-  <>
-    <button className={styles.activeButton} onClick={() => setToggleList(true)}>Videos</button>
-    <button className={styles.inactiveButton} onClick={() => setToggleList(false)}>Articles</button>
-  </> : 
-  <>
-    <button className={styles.inactiveButton} onClick={() => setToggleList(true)}>Videos</button>
-    <button className={styles.activeButton} onClick={() => setToggleList(false)}>Articles</button>
-  </>
-
-
-
+    <>
+      <button className={styles.activeButton} onClick={() => setToggleList(true)}>Videos</button>
+      <button className={styles.inactiveButton} onClick={() => setToggleList(false)}>Articles</button>
+    </> : 
+    <>
+      <button className={styles.inactiveButton} onClick={() => setToggleList(true)}>Videos</button>
+      <button className={styles.activeButton} onClick={() => setToggleList(false)}>Articles</button>
+    </>
 
   return (
   <div className={styles.actIdeasContainer}>
 
     <div className={styles.btnContainer}>
-        {showButtons}
-      </div>
+      {showButtons}
+    </div>
+
+    {/* filter headings need to go here */}
+
+    <div className={styles.filterTagsContainer}>
+       <ul className={styles.filterTags}>
+        <li><a href="">ALL</a></li>
+        <li><a href="">INDOOR</a></li>
+        <li><a href="">OUTDOOR</a></li>
+        <li><a href="">SOCIAL SKILLS</a></li>
+        <li><a href="">LANGUAGE</a></li>
+        <li><a href="">STRENGTH</a></li>
+       </ul>
+     </div>
+
 
     <div >
       {showClickedList}
     </div>
+
+     
 
   </div>
   );
