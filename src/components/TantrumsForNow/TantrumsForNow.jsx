8de5a8e5b78c701b15fbfb422a6322476a040ad3
Joseph McGrath <joseph.mcgrath@engineer.com>
2020-10-28 15:47:25 +0000

    Add: Styling For Now Page

--- src/components/TantrumsForNow/TantrumsForNow.jsx
+++ src/components/TantrumsForNow/TantrumsForNow.jsx
@@ -11,17 +11,33 @@ const TantrumsForNow = () => {
   return (
     <>
       <div className={styles.whatNotToDo}>
-          <h3>What NOT to do:</h3>
-          <ul className={styles.topUl}>
-            <li><FontAwesomeIcon icon={faTimes} />Yell or spank</li>
-            <li><FontAwesomeIcon icon={faTimes} />Give in just because they are crying</li>
-            <li><FontAwesomeIcon icon={faTimes} />Mock or threaten</li>
-            <li><FontAwesomeIcon icon={faCircle} size="0.1x" className={styles.circle}/>Try to reason with your kid or apply logic</li>
-          </ul>
+        <h3>What NOT to do:</h3>
+        <ul className={styles.whatNotToDoUl}>
+          <li>
+            <FontAwesomeIcon icon={faTimes} />
+            Yell or spank
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faTimes} />
+            Give in just because they are crying
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faTimes} />
+            Mock or threaten
+          </li>
+          <li>
+            <FontAwesomeIcon
+              icon={faCircle}
+              size="0.1x"
+              className={styles.circle}
+            />
+            Try to reason with your kid or apply logic
+          </li>
+        </ul>
       </div>
       <div className={styles.tryThis}>
         <h3>Try this:</h3>
-        <ul>
+        <ul className={styles.tryThisUl}>
           <li>Take a deep breath and calm down</li>
           <li>Remove your child from a situation, if needed</li>
           <li>Be firm but gentle</li>
