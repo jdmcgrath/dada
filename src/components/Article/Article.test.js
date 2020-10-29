6a8c3b6c822dbb73864cf6876b8c281abb6bbb86
Mark Birchall <mark.birchall@gmail.com>
2020-10-29 14:45:22 +0000

    tests removed

--- src/components/Article/Article.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Article from "./Article";
-
-describe("Article tests", () => {
-  it("should render", () => {
-    expect(render(<Article />)).toBeTruthy();
-  });
-});
--- src/components/Video/Video.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Video from "./Video";
-
-describe("Video tests", () => {
-  it("should render", () => {
-    expect(render(<Video />)).toBeTruthy();
-  });
-});
