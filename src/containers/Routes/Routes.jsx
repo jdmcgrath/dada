00788e2991b21e049069e0ce5e7d161bd5247af4
Frank McNulty <frank.mcnulty@nology.io>
2020-10-29 16:56:50 +0000

    added missing routes

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -8,8 +8,8 @@ import BookInfo from "../../components/BookInfo";
 import AddChick from "../../components/AddChick";
 import SOS from "../../components/SOS";
 import SplashScreen from "../../components/SplashScreen";
-// import Tantrums from "../../components/Tantrums"
-// import ArticleReader from "../../components/ArticleReader";
+import Tantrums from "../../components/Tantrums"
+import ArticleReader from "../../components/ArticleReader";
 import Welcome from "../../components/Welcome";
 import AddChickAge from "../../components/AddChickAge";
 import VideoList from '../../components/VideoList';
@@ -27,6 +27,8 @@ const Routes = () => {
             <ActivityIdeas path="categories/activity-ideas" />
             <VideoList path="categories/activity-ideas/video-list" />
             <ArticleList path="categories/activity-ideas/article-list" />
+            <Tantrums path="/categories/sos/tantrums" />
+            <ArticleReader path="article-reader" />
             <BookSmarts path="book-smarts" />
             <BookInfo path="book-smarts/book-info" />
             <SOS path="/categories/sos"/>
