5ce8acafe46ca52daa0749969d59ffbd7e92f80c
Mark Birchall <mark.birchall@gmail.com>
2020-11-03 15:30:49 +0000

    filters with andy

--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import Article from '../Article/Article';
 import styles from './ArticleList.module.scss';
 
@@ -6,25 +7,32 @@ const ArticleList = (props) => {
 
   const { filterChosen, articles } = props;
 
+  // state
+  const [filteredArticles, setFilteredArticles] = useState([]);
+
+  // lifecycle/hooks
+  useEffect(() => {
+    let filteredArticles = articles;
+
+    // first check if there are no filters... because we don't want to filter when there aren't
+    if (filterChosen) {
+      // take the Articles and filter them if they match the fitlers we have
+      filteredArticles = articles.filter(a => a.keywords.indexOf(filterChosen) > -1);
+    }
+
+    // we have our filtered Articles we love it... lets map these into JSX elements so they print something on the page puuuleaseee.
+    const articleElements = filteredArticles.map(article => <Article article={article} />);
+
+    // Update the Articles in our state so that the page re-renders....
+    setFilteredArticles(articleElements);
+
+  }, [filterChosen, articles]);
+
+  // return
   return (
-    <div className={styles.articleListContainer}>
-
-
-
-      {articles.map((article) => {
-        for (let i = 0; i < article.keywords.length; i++) {
-          if (article.keywords[i] === filterChosen || filterChosen === "") {
-            return <Article article={article} />
-          }else{
-            return null
-          }
-        }
-      })}
-
-
-
-    </div>
-
+      <div className={styles.articleListContainer}>
+        {filteredArticles}
+      </div>
   )
 }
 
--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,28 +1,39 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 import Video from '../Video'
 import styles from './VideoList.module.scss';
 
 const VideoList = (props) => {
 
+  // props
   const { filterChosen, videos } = props;
 
+  // state
+  const [filteredVideos, setFilteredVideos] = useState([]);
 
+  // lifecycle/hooks
+  useEffect(() => {
+    let filteredVideos = videos;
+
+    // first check if there are no filters... because we don't want to filter when there aren't
+    if (filterChosen) {
+      // take the videos and filter them if they match the fitlers we have
+      filteredVideos = videos.filter(v => v.keywords.indexOf(filterChosen) > -1);
+    }
+
+    // we have our filtered videos we love it... lets map these into JSX elements so they print something on the page puuuleaseee.
+    const videoElements = filteredVideos.map(video => <Video video={video} />);
+
+    // Update the videos in our state so that the page re-renders....
+    setFilteredVideos(videoElements);
+
+  }, [filterChosen, videos]);
+
+  // return
   return (
-
       <div className={styles.vidListContainer}>
-
-        {videos.map((video) => {
-          for (let i = 0; i < video.keywords.length; i++) {
-            if (video.keywords[i] === filterChosen || filterChosen === "") {
-              return <Video video={video} />
-            }else{
-              return null
-            }
-          }
-        })}
-
+        {filteredVideos}
       </div>
-
   )
 }
 
