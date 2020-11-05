13b570752cb352c76c529b09d278b632b684dc5e
Frank McNulty <frank.mcnulty@nology.io>
2020-11-05 16:42:30 +0000

    firestore updates forvarious components

--- src/components/UpYourGame/UpYourGame.jsx
+++ src/components/UpYourGame/UpYourGame.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { upYourGameCardData } from "../../data/upYourGameData";
-import { mostDiscussedData } from "../../data/upYourGameData";
 import Styles from "./UpYourGame.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import BottomNavBar from "../BottomNavBar";
-
+import {firestore} from "../../firebase";
+import { useEffect, useState } from 'react';
 
 const UpYourGame = () => {
+  const [docs, setDocs] = useState([]);
+  const [subDocs, setSubDocs] = useState([]);
+  const getUpYourGameMainPage = () => {
+    firestore.collection("upYourGameMainPage").get().then((response) => {
+       const documents = response.docs.map(d => d.data());
+       setDocs(documents) 
+    })
+  } 
+  useEffect (() => {
+    getUpYourGameMainPage();
+  }, [])
+  const getUpYourGameMostDiscussed = () => {
+    firestore.collection("upYourGameMostDiscussed").get().then((response) => {
+       const documents = response.docs.map(d => d.data());
+       setSubDocs(documents) 
+    })
+  } 
+  useEffect (() => {
+    getUpYourGameMostDiscussed();
+  }, [])
   return (
     <>
     <header className={Styles.smHeader}>
@@ -18,16 +37,16 @@ const UpYourGame = () => {
     
 
 
-      {upYourGameCardData.map((UpYourGameCardData) => {
+      {docs.map((doc) => {
         return(
            <section className={Styles.upYourGameArticles}>
 
-            <img className={Styles.articleBackgroundImage} src={UpYourGameCardData.backgroundImage} alt=""/>
-            <img className={Styles.articleProfileImage} src={UpYourGameCardData.profileImage} alt= "" />
-            <button className={Styles.articleTag}>{UpYourGameCardData.filterTag}</button>
-            <h2>{UpYourGameCardData.title}</h2>
-            <h3>{UpYourGameCardData.subtitle}</h3>
-            <p>{UpYourGameCardData.author} | {UpYourGameCardData.readTime}</p>
+            <img className={Styles.articleBackgroundImage} src={doc.backgroundImage} alt=""/>
+            <img className={Styles.articleProfileImage} src={doc.profileImage} alt= "" />
+            <button className={Styles.articleTag}>{doc.filterTag}</button>
+            <h2>{doc.title}</h2>
+            <h3>{doc.subtitle}</h3>
+            <p>{doc.author} | {doc.readTime}</p>
             <p><FontAwesomeIcon icon={"comment-alt"}/> </p>
 
            </section>
@@ -35,12 +54,12 @@ const UpYourGame = () => {
         })}
 
         <h2>Most Discussed</h2>
-          {mostDiscussedData.map((MostDiscussedCards) => {
+          {subDocs.map((subDoc) => {
             return(
           <section className={Styles.mostDiscussedCards}>  
-            <h3>{MostDiscussedCards.title}</h3>
-            <span>{MostDiscussedCards.author} | </span>
-            <span>{MostDiscussedCards.readTime}</span>
+            <h3>{subDoc.title}</h3>
+            <span>{subDoc.author} | </span>
+            <span>{subDoc.readTime}</span>
           </section>
           )})}
         </main> 
