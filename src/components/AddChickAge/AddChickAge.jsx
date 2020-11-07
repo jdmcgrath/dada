bab6c97f0b1f05be0ad8682799c647dd5f4dc19a
Mark Birchall <mark.birchall@gmail.com>
2020-11-07 14:54:21 +0000

    add chick page and primarybtn color

--- src/components/AddChickAge/AddChickAge.jsx
+++ src/components/AddChickAge/AddChickAge.jsx
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { useState} from "react";
 import styles from "./AddChickAge.module.scss";
+import BoyChick from "../../assets/img/AddChick/BoyChick.svg";
+import GirlChick from "../../assets/img/AddChick/GirlChick.svg";
 
 
 
 const AddChickAge = () => {
+
+  const [ chickAge, setChickAge ] = useState(0);
+
+  const increaseAge = () => {
+    setChickAge(chickAge+1);
+  }
+
+  const decreaseAge = () => {
+    if(chickAge>0){
+      setChickAge(chickAge-1);
+    } 
+  }
+
   return (
     <div className={styles.pageContainer}>
-            {/* <header className={styles.header}> */}
-                <h2>How old is your chick?</h2>
-                             {/* <div className={styles.imageContainer}> */}
-                    {/* <img className={styles.circularImg} src="" alt=" " srcset=""/> */}
-                    {/* <span class={styles.faIcon}>
-                        <FontAwesomeIcon icon={faPlusCircle} />
-                    </span> */}
-                {/* </div>       */}
-                {/* </header> */}
+       <header className={styles.header}>
+
+        {/* girl or boy passed as prop from AddChick */}
+        <img src={GirlChick} alt="Penguin-placeholder-img"/> 
+
+       </header>
+               
+       {/* name of Chick as prop from AddChick */}
+       <h2>How old is ChickName?</h2>
+
+      <div className={styles.ageClicker}>
+        <button className={styles.minusButton} onClick={decreaseAge}>-</button>
+        <span className={styles.ageNumber}>{chickAge}</span>
+        <button className={styles.plusButton} onClick={increaseAge}>+</button>
+      </div>
+
+      {/* <Link to="./AddChickEnd"> */}
+      <button type="submit" className={styles.primaryBtn}>Next</button>
+      {/* </Link> */}
+
     </div>
   );
 };
+
+// chekcing - and +
 
 export default AddChickAge;
