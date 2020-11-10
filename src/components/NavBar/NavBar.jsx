e27576d43c5fae72799d150413b3351ee8ffebe2
Kuda11 <kudathomad17@yahoo.com>
2020-11-10 14:54:08 +0000

    back button

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { navigate } from '@reach/router'
 import styles from "./NavBar.module.scss"
 
 const NavBar = () => { 
+    const handleBackButton = () => {
+        if (window.location.pathname === "/categories") {
+            return
+        } else {
+            navigate(-1)     
+        }
+
+    }
     return ( 
         <>
             <header className={styles.navWrapper}>
                 <nav className={styles.globalNav}>
-                    <FontAwesomeIcon icon={faChevronLeft} className={styles.backButton}/>
+                    <FontAwesomeIcon icon={faChevronLeft} onClick={handleBackButton} className={styles.backButton}/>
                     {/* <p className={styles.pageTitle}>Page Title</p> */}
                     <FontAwesomeIcon icon={faEllipsisV} className={styles.ellipses}/>
                 </nav>
