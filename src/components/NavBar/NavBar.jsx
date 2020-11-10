3119f27cc6a86e46a6e29b300667647dfd3f9245
Kuda11 <kudathomad17@yahoo.com>
2020-11-10 15:05:41 +0000

    signout button

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,11 +14,20 @@ const NavBar = () => {
         }
 
     }
+
+    const handlesSignOut = () => {
+        firebase.auth().signOut().then(function() {
+            alert("You SIGNED OUT!!!")
+          }).catch(function(error) {
+              alert("error")
+          });
+    }
     return ( 
         <>
             <header className={styles.navWrapper}>
                 <nav className={styles.globalNav}>
                     <FontAwesomeIcon icon={faChevronLeft} onClick={handleBackButton} className={styles.backButton}/>
+                    <FontAwesomeIcon icon={faChevronLeft} onClick={handlesSignOut} className={styles.backButton}/>
                     {/* <p className={styles.pageTitle}>Page Title</p> */}
                     <FontAwesomeIcon icon={faEllipsisV} className={styles.ellipses}/>
                 </nav>
