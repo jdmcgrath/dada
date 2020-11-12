0b328de8f5be0244260e7d0307fd61cc230d629a
Steve Maguire <dannyblack42@protonmail.com>
2020-11-12 17:38:26 +0000

    edit: BS 1st page @media done

--- src/components/BookSmarts/BookSmartCard/BookSmartCard.jsx
+++ src/components/BookSmarts/BookSmartCard/BookSmartCard.jsx
@@ -1,53 +1,61 @@
-import React, {useState} from 'react';
-import styles from './BookSmartCard.module.scss';
-import { Link } from '@reach/router';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from "react";
+import styles from "./BookSmartCard.module.scss";
+import { Link } from "@reach/router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowCircleUp } from "@fortawesome/free-solid-svg-icons";
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-
-
-
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 const BookSmartCard = (props) => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    console.log(props)
+  console.log(props);
 
-    const { BookId,
-            Title, 
-            Author,
-            publishDate,
-            Img } = props.doc;
+  const { BookId, Title, Author, publishDate, Img } = props.doc;
 
-    const increaseCount = () => {
-        setCount(addCount => addCount + 1)
-    }
+  const increaseCount = () => {
+    setCount((addCount) => addCount + 1);
+  };
 
-    return (
+  return (
+    <section className={styles.cardContainer}>
+      <div className={styles.bookInfoContainer}>
+        <div className={styles.bookSmartInfo}>
+          <h2 className={styles.bookTitle}>{Title}</h2>
+          <p className={styles.bookAuthor}>
+            {Author}, {publishDate}
+          </p>
+        </div>
+        <div className={styles.bookSmartVotes}>
+          <span onClick={increaseCount} className={styles.voteIcon}>
+            <FontAwesomeIcon icon={faArrowCircleUp} />
+          </span>
+          <h5 className={styles.voteCounter}>{count}</h5>
+          {/* <span onClick={reduceCount} className={styles.voteIcon}><FontAwesomeIcon icon={faArrowCircleDown} /></span> */}
+        </div>
+      </div>
+
+      <Link
+        to={`book-info/${BookId}`}
+        doc={props.doc}
         
-        <section className={styles.cardContainer}>
-            <div className={styles.bookInfoContainer}>
-                <div className={styles.bookSmartInfo}>
-                    <h2 className={styles.bookTitle}>{Title}</h2>
-                    <p className={styles.bookAuthor}>{Author}, {publishDate}</p>
-                </div>
-                <div className={styles.bookSmartVotes}>
-                    <span onClick={increaseCount} className={styles.voteIcon}><FontAwesomeIcon icon={faArrowCircleUp} /></span>
-                    <h5 className={styles.voteCounter}>{count}</h5>
-                    {/* <span onClick={reduceCount} className={styles.voteIcon}><FontAwesomeIcon icon={faArrowCircleDown} /></span> */}
-                </div> 
-            </div> 
-
-            <Link to={`book-info/${BookId}`} doc={props.doc} className={styles.bookSmartImageContainer}>
-            <div className={styles.bookSmartImage}>
-                <img src={Img} alt="book-cover" />
-            </div>  
-            </Link>
-            <span className={styles.rightChevron}><FontAwesomeIcon icon={faChevronRight} /></span>
-        </section>
-    
-    )
-} 
+      >
+        <div className={styles.bookSmartImage}>
+          <img src={Img} alt="book-cover" />
+        </div>
+      </Link>
+      <Link
+        to={`book-info/${BookId}`}
+        doc={props.doc}
+      ></Link>
+      <Link
+        to={`book-info/${BookId}`}
+        doc={props.doc}
+        className={styles.rightChevron}
+      >
+        <FontAwesomeIcon icon={faChevronRight} />
+      </Link>
+    </section>
+  );
+};
 
 export default BookSmartCard;
-
