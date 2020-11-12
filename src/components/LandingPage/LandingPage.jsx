e964303e592e5e65076b2140a353ea7a4cf9bece
James <james.loizou@nology.io>
2020-11-12 09:41:43 +0000

    morning work

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -14,12 +14,6 @@ const LandingPage = () => {
       <div className={styles.splash}>
         <img src={splash} alt="splash" />
         <button>Login</button>
-        {/* 
-                    <Link to = "categories">
-                    <button className={styles.dadviceBtn}>Get Dadvice</button>
-                    </Link>
-                    
-                <p className={styles.underButtonText}>Already have an account? <span>Log in</span> or <Link to ="sign-up"><span>Sign up</span></Link></p> */}
       </div>
 
       <div className={styles.pageCont}>
@@ -79,29 +73,13 @@ const LandingPage = () => {
           </li>
         </ul>
       </div>
+      <div className={styles.endOfPage}>
+        <button className={styles.primaryBtn}>Sign Up</button>
+        <h2 className={styles.endOfPageText}>Contact Us</h2>
+        <h2 className={styles.endOfPageText}>Privacy</h2>
+        <h2 className={styles.endOfPageText}>Terms</h2>
+      </div>
 
-      {/* <div className={styles.textUnderSplash}> 
-                <p>Most parenting resources are built with moms in mind.</p>
-                <p>We’ve created one for you, dad! </p>
-            </div>
-            <div className={styles.textBeforeTrailing}>
-                <p>Our mission is to empower busy fathers to parent with confidence and enjoy more quality time with their kids.</p>
-            </div>
-            <img src={trailingBabies}/>
-            <h2>Here you will find:</h2>
-            <ul> 
-                <li><p>Fast and clear answers to most common parenting challenges</p></li>
-                <li><p>Low-effort activity ideas to suit your mood and budget</p></li>
-                <li><p>Cliff-notes from top-rated parenting books (because who has the time to read the whole thing?)</p></li>
-                <li><p>Longer reads for the wait-but-why dads out there</p></li>
-            </ul>
-            <div>
-                <Link to="sign-up">
-                <button>Sign Up</button>
-                </Link>
-                <p className={styles.logIn}>Already have an account? <span><a href="https://github.com/nology-tech/dada">Login</a></span></p>
-                <p className={styles.orSignIn}>or sign in with</p>
-            </div> */}
     </div>
   );
 };
