c0acdc385d62294baeae3970cbf22895f2aadc5c
Mark Birchall <mark.birchall@gmail.com>
2020-11-13 16:31:09 +0000

    sidebar fix

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -68,7 +68,6 @@ const Routes = (props) => {
             <Whining path="/categories/sos/whining" user={user} />
             <UpYourGame path="/categories/up-your-game" user={user} />
             <ProfileSignIn path="/profile-sign-in" />
-            <Favorites path="favourites" user={user} />
         </Router>
     )
 };
