12f52358e0bd9ef01f334cb34bed40d9a57f1fe9
Kuda11 <kudathomad17@yahoo.com>
2020-11-13 14:48:10 +0000

    november 13 changies

--- src/components/SignUp/Login/Login.jsx
+++ src/components/SignUp/Login/Login.jsx
@@ -4,7 +4,7 @@ import { navigate } from '@reach/router'
 import LoginSocialFollow from './LoginSocialFollow';
 import firebase from "../../../firebase"
 
-export const Login = () => {
+export const Login = (props) => {
   const passwordRef = useRef()
   const emailRef = useRef()
 
@@ -15,29 +15,32 @@ export const Login = () => {
       await firebase
         .auth()
         .signInWithEmailAndPassword(emailRef.current.value, passwordRef.current.value)
-      navigate("/categories")
     }
     catch (error) {
       alert(error)
     }
   }
-  const handleSignUpPage = (e) => {
-    e.preventDefault();
-    navigate('/sign-up')
-  }
+
   const getUser = () => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         navigate("/categories");
       } else {
+        console.log('else')
         return
       }
     });
   };
 
+  const handleSignUpPage = (e) => {
+    e.preventDefault();
+    navigate('/sign-up')
+  }
+
   useEffect(() => {
-    getUser();
-  })
+    console.log("user running")
+    getUser();   
+  }, [])
 
   return (
     <form className={styles.formContainer} onSubmit={handleLogin}>
